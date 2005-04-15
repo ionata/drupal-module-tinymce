0229b1129258d6061393ede13d6b51cdba5b1836
Matt Westgate <matt_westgate@2275.no-reply.drupal.org>
2005-04-15 19:08:36 +0000

    * Uploading images now works as expected in IE without any 'This browser has lost communication...' errors. The image upload form also opens in the correct window now. You'll need to update the drupalimage plugin.

--- plugins/drupalimage/editor_plugin.js
+++ plugins/drupalimage/editor_plugin.js
@@ -5,32 +5,37 @@ tinyMCE.importPluginLanguagePack('drupalimage', 'en');
  * Returns the HTML contents of the emotions control.
  */
 function TinyMCE_drupalimage_getControlHTML(control_name) {
-	switch (control_name) {
-		case "drupalimage":
-			return '<img id="{$editor_id}_drupalimage" src="{$pluginurl}/images/camera.png" title="{$lang_drupalimage_desc}" width="20" height="20" class="mceButtonNormal" onmouseover="tinyMCE.switchClass(this,\'mceButtonOver\');" onmouseout="tinyMCE.restoreClass(this);" onmousedown="tinyMCE.restoreAndSwitchClass(this,\'mceButtonDown\');" onclick="tinyMCE.execInstanceCommand(\'{$editor_id}\',\'mceDrupalimage\');">';
-	}
+  switch (control_name) {
+    case "drupalimage":
+      return '<img id="{$editor_id}_drupalimage" src="{$pluginurl}/images/camera.png" title="{$lang_drupalimage_desc}" width="20" height="20" class="mceButtonNormal" onmouseover="tinyMCE.switchClass(this,\'mceButtonOver\');" onmouseout="tinyMCE.restoreClass(this);" onmousedown="tinyMCE.restoreAndSwitchClass(this,\'mceButtonDown\');" onclick="tinyMCE.execInstanceCommand(\'{$editor_id}\',\'mceDrupalimage\');">';
+  }
 
-	return "";
+  return "";
 }
 
 /**
  * Executes the mceEmotion command.
  */
 function TinyMCE_drupalimage_execCommand(editor_id, element, command, user_interface, value) {
-	// Handle commands
-	switch (command) {
-		case "mceDrupalimage":
-			var template = new Array();
+  // Handle commands
+  switch (command) {
+    case "mceDrupalimage":
+      var template = new Array();
 
-			template['file'] = 'index.php?q=img_assist/add&editor=tinymce'; // Relative to theme
-			template['width'] = 500;
-			template['height'] = 660;
+      template['file'] = 'index.php?q=img_assist/add&editor=tinymce'; // Relative to theme
+      template['width'] = 500;
+      template['height'] = 660;
 
-			tinyMCE.openWindow(template, {editor_id : editor_id});
+      //WARNING: "resizable : 'yes'" below is painfully important otherwise
+      // tinymce will try to open a new window in IE using showModalDialog().
+      // And for some reason showModalDialog() doesn't respect the target="_top"
+      // attribute.
 
-			return true;
-	}
+      tinyMCE.openWindow(template, {editor_id : editor_id, resizable : 'yes', scrollbars : 'yes'});
 
-	// Pass to next handler in chain
-	return false;
+      return true;
+  }
+
+  // Pass to next handler in chain
+  return false;
 }
