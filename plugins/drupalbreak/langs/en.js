9c4ef43f12b8a1fbcf357bbe0b6b5d84c284ad94
Ted Serbinski <m3avrck@12932.no-reply.drupal.org>
2005-11-30 23:14:56 +0000

    added new drupalbreak plugin to work with this pagination module: http://drupal.org/node/23362#comment-43694

---
+++ plugins/drupalbreak/langs/en.js
@@ -0,0 +1,11 @@
+// UK lang variables
+
+tinyMCE.addToLang('drupalbreak',{
+title  : 'Insert Teaser Break',
+desc   : 'Seperate Drupal Teaser and Body'
+});
+
+tinyMCE.addToLang('drupalpagebreak',{
+title  : 'Insert Page Break',
+desc   : 'Seperate Pages'
+});
