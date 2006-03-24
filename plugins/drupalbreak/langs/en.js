e125b42fb8351615ae92280970b5360054a67be3
Ted Serbinski <m3avrck@12932.no-reply.drupal.org>
2006-03-24 06:56:19 +0000

    Patch #54713 : spelling fix

--- plugins/drupalbreak/langs/en.js
+++ plugins/drupalbreak/langs/en.js
@@ -2,10 +2,10 @@
 
 tinyMCE.addToLang('drupalbreak',{
 title  : 'Insert Teaser Break',
-desc   : 'Seperate Drupal Teaser and Body'
+desc   : 'Separate Drupal Teaser and Body'
 });
 
 tinyMCE.addToLang('drupalpagebreak',{
 title  : 'Insert Page Break',
-desc   : 'Seperate Pages'
+desc   : 'Separate Pages'
 });
